14b90e6bf8159c32b798e1bfcf3f9be79f232b0b
adamkr98 <adamkrynski2@gmail.com>
2023-10-16 13:45:33 +0200

    clearing the input after submission

--- src/components/MainPublicPage/SignIn.jsx
+++ src/components/MainPublicPage/SignIn.jsx
@@ -67,19 +67,62 @@ const SignIn = ({ closeSignInModal }) => {
   };
 
   return (
-    // ... (rest of your component code)
+    <div className="w-full relative flex justify-center items-center">
+      {!isClosed && (
+        <div className="w-[40rem] max-w-[90%] h-[70vh] bg-[#e8ebee] md:bg-green-400 rounded-sm">
+          <div className='w-full mt-8 mb-4 flex justify-center'>
+            <p className='text-2xl'>Hello, Nice to meet You!</p>
+          </div>
 
-    <input type="email" id="email" className="w-[80%] h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" required ref={emailRef} value={email} onChange={(e) => setEmail(e.target.value)} />
+          <form action="" className="w-full h-fit flex flex-col items-center">
+            <div className="w-[80%] mb-4 mt-4 flex flex-col items-center">
+              <label htmlFor="email" className="w-[80%] pb-2">
+                Email
+              </label>
+              <input type="email" id="email" className="w-[80%] h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" required ref={emailRef} value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
 
-    <input type="password" ref={passwordRef} className="w-[90%] h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" required value={password} onChange={(e) => setPassword(e.target.value)} />
+            <div className="w-[80%] mb-4 flex justify-center">
+              <div className='w-[40%] flex flex-col items-start'>
+                <label htmlFor="password" className="pb-2">
+                  Password
+                </label>
+                <input type="password" ref={passwordRef} className="w-[90%] h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className='w-[40%] flex flex-col items-end'>
+                <label htmlFor="password-confirm" className="w-[90%] pb-2">
+                  Password Confirmation
+                </label>
+                <input type="password" className="w-[90%] h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" />
+              </div>
+            </div>
 
-    <input type="text" id="userName" className="w-full h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" ref={userNameRef} value={userName} onChange={(e) => setUserName(e.target.value)} />
+            <div className="w-[70%] mb-4 flex justify-around">
+              <div className='w-[40%] flex flex-col'>
+                <label htmlFor="username" className="pb-2">
+                  User Name
+                </label>
+                <input type="text" id="userName" className="w-full h-[2.5rem] pl-4 rounded-md border-b border-b-red-600" ref={userNameRef} value={userName} onChange={(e) => setUserName(e.target.value)} />
+              </div>
+              <div className="w-[40%] mb-4 flex flex-col items-center">
+                <label htmlFor="role" className='w-full pb-2'>Role</label>
+                <select id="role" className="w-full h-[2.5rem] bg bg-white" ref={roleRef} value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option>Buyer</option>
+                  <option>Seller</option>
+                </select>
+              </div>
+            </div>
 
-    <select id="role" className="w-full h-[2.5rem] bg bg-white" ref={roleRef} value={role} onChange={(e) => setRole(e.target.value)}>
-      {/* ... (options) */}
-    </select>
-
-    // ... (rest of your component code)
+            <button type="button" onClick={handleRegister} className="bg bg-[#606C38] pb-2 pt-2 pl-4 pr-4 hover:bg-[#7f8f49] mb-4 rounded-md text-white">
+              SignUp
+            </button>
+          </form>
+          <button onClick={toggleCloseSignIn} className="absolute top-0 right-0">
+            x
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
